7cb4a5cbefca6a027be0caf1f4c1543cb5a6f798
Souratendu <souratendu@gmail.com>
2019-09-08 16:15:44 -0400

    Added codes for my final project

---
+++ Train and Play Lunar Lander using DQN/lunarlander-trained.py
@@ -0,0 +1,35 @@
+import gym
+import random
+import torch
+import numpy as np
+from collections import deque
+import matplotlib.pyplot as plt
+
+
+env = gym.make('LunarLander-v2')
+env.seed(0)
+print('State shape: ', env.observation_space.shape)
+print('Number of actions: ', env.action_space.n)
+
+from dqn_agent import Agent
+
+agent = Agent(state_size=8, action_size=4, seed=0)
+
+
+
+# load the weights from file
+agent.qnetwork_local.load_state_dict(torch.load('checkpoint.pth'))
+
+for i in range(3):
+    state = env.reset()
+    score=0 
+    for j in range(200):
+        action = agent.act(state)
+        env.render()
+        state, reward, done, _ = env.step(action)
+        score+=reward
+        if done:
+            break 
+    print('Score: %d' %score)
+            
+env.close()
